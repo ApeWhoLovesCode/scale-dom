d750abc69c44147990d34181fb58e435b267f453
木亘 <76390957+ApeWhoLovesCode@users.noreply.github.com>
2025-03-15 01:02:11 +0800

    fix: 实现点击左侧导航栏重新监听实现缩放

--- content.tsx
+++ content.tsx
@@ -4,66 +4,81 @@ import { INIT_STATE } from "~common/state"
 import { DOM_KEY, IS_OPEN } from "~common/storage"
 import { useStorage } from "~node_modules/@plasmohq/storage/dist/hook"
 
+// test
+// https://u.pmdaniu.com/xn98z
+
+/**
+ * 待解决问题
+ * 1.放大后，部分内容丢失
+ * 
+ */
+
 export default function Content() {
   const [isOpen] = useStorage<boolean>(IS_OPEN, INIT_STATE.isOpen)
   const [domKey] = useStorage<string>(DOM_KEY, INIT_STATE.domKey)
-  const scrollDom = useRef(null)
-  const scaleV = useRef(1)
+  // const scrollDom = useRef(null)
+  // const scaleV = useRef(1)
 
   useEffect(() => {
     if (!isOpen) return
+    let onMousewheel: (e: WheelEvent) => void
     let wheelTimer: NodeJS.Timeout | null = null
+    let scaleV = 1;
 
     let iframe: HTMLIFrameElement
     let iframeDocument: Document
-    const getIframe = () => {
+    let scrollDom: HTMLElement
+
+    const loopOnIframe = (c: number) => {
+      onIframeWheel()
+      if (scrollDom || c++ > 10) return
+      setTimeout(() => {
+        loopOnIframe(c)
+      }, 500);
+    }
+
+    // 点击左侧导航栏，重新加载获取 iframe 中的元素实现监听。
+    const navLi = document.querySelectorAll('.sitemapNode')
+    navLi.forEach((li) => {
+      li.addEventListener('click', () => {
+        iframe = null
+        loopOnIframe(0)
+      })
+    })
+
+    const onIframeWheel = () => {
+      if(onMousewheel) {
+        iframeDocument.removeEventListener("mousewheel", onMousewheel)
+      }
+
       iframe = document.querySelector(domKey) as HTMLIFrameElement
       iframeDocument = iframe?.contentDocument
-    }
-    getIframe()
-    if(!iframe || !iframeDocument) return
-
-    const iframeBody = iframeDocument?.querySelector('body')
-    if (iframeBody) {
-      scrollDom.current = iframeBody
-    }
-    // let isCtrl = false;
-    // const onKeyDown = (e: KeyboardEvent) => {
-    //   if(e.ctrlKey) {
-    //     e.stopPropagation()
-    //     e.preventDefault()
-    //     isCtrl = true
-    //   }
-    // }
-
-    // const onkeyUp = (e: KeyboardEvent) => {
-    //   if(!e.ctrlKey) {
-    //     isCtrl = false
-    //   }
-    // }
-
-    const onMousewheel = (e: WheelEvent) => {
-      const isCtrl = e.ctrlKey
-      if (!scrollDom.current || !isCtrl) return
-      // e.stopPropagation()
-      e.preventDefault();
-      if (wheelTimer) {
-        clearTimeout(wheelTimer)
+      scrollDom = iframeDocument?.querySelector('body')
+      if(!scrollDom) return
+  
+      onMousewheel = (e: WheelEvent) => {
+        const isCtrl = e.ctrlKey
+        if (!scrollDom || !isCtrl) return
+        // e.stopPropagation()
+        e.preventDefault();
+        if (wheelTimer) {
+          clearTimeout(wheelTimer)
+        }
+        wheelTimer = setTimeout(() => {
+          const sv = e.deltaY > 0 ? -1 : 1
+          scaleV += sv * 0.1
+          scrollDom.style.transform = `scale(${scaleV})`
+        }, 10)
       }
-      wheelTimer = setTimeout(() => {
-        const sv = e.deltaY > 0 ? -1 : 1
-        scaleV.current += sv * 0.1
-        scrollDom.current.style.transform = `scale(${scaleV.current})`
-      }, 10)
-    }
 
-    // window.addEventListener("keydown", onKeyDown);
-    // window.addEventListener("keyup", onkeyUp);
-    // nav 点击时要重新监听 iframe
-    iframeDocument.addEventListener("mousewheel", onMousewheel, { passive: false })
+      // nav 点击时要重新监听 iframe
+      iframeDocument.addEventListener("mousewheel", onMousewheel, { passive: false })
+    }
+    onIframeWheel()
+
+
+    // iframeDocument.addEventListener("mousewheel", onMousewheel, { passive: false })
     return () => {
-      // window.removeEventListener("keydown", onKeyDown);
-      // window.removeEventListener("keyup", onkeyUp);
       iframeDocument.removeEventListener("mousewheel", onMousewheel)
     }
   }, [isOpen, domKey])
@@ -79,19 +94,6 @@ export default function Content() {
       window.removeEventListener("mousewheel", onMousewheel)
     }
   }, [isOpen])
-
-  // useEffect(() => {
-  //   // https://u.pmdaniu.com/xn98z
-  //   setTimeout(() => {
-  //     const iframe = document.querySelector(domKey) as HTMLIFrameElement
-  //     const iframeDocument = iframe?.contentDocument
-  //     const dom = iframeDocument?.querySelector('body')
-  //     console.log("====scrollDom: ", dom)
-  //     if (dom) {
-  //       scrollDom.current = dom
-  //     }
-  //   }, 100);
-  // }, [domKey])
 
   return <></>
 }
--- popup/index.tsx
+++ popup/index.tsx
@@ -4,7 +4,7 @@ import { useStorage } from "@plasmohq/storage/hook"
 import { DOM_KEY, IS_OPEN } from "~common/storage"
 
 import Radio from "./components/Radio"
-import Button from "./components/Button"
+// import Button from "./components/Button"
 
 import "./style.css"
 import { INIT_STATE } from "~common/state"
@@ -18,10 +18,10 @@ function IndexPopup() {
     setDomKeyState(domKey)
   }, [domKey])
 
-  const setDefault = () => {
-    setDomKeyState(INIT_STATE.domKey)
-    setDomKey(INIT_STATE.domKey)
-  }
+  // const setDefault = () => {
+  //   setDomKeyState(INIT_STATE.domKey)
+  //   setDomKey(INIT_STATE.domKey)
+  // }
 
   return (
     <div className="bg-black text-white p-4 w-64 text-sm select-none">
@@ -29,7 +29,7 @@ function IndexPopup() {
         <Radio checked={isOpen} onChange={setIsOpen} />
         <span className="ml-2">启用插件</span>
       </div>
-      <div className="mb-1">选择元素</div>
+      {/* <div className="mb-1">选择元素</div>
       <input
         value={domKeyState}
         onChange={(e) => setDomKeyState(e.target.value)}
@@ -39,7 +39,7 @@ function IndexPopup() {
       />
       <div className="mt-2">
         <Button className="text-xs" onClick={setDefault}>恢复默认</Button>
-      </div>
+      </div> */}
     </div>
   )
 }
